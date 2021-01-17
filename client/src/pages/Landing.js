58fd19dd40e16896c4e5494ee0359b0defab2018
nartexyu <70301421+nartexyu@users.noreply.github.com>
2021-01-17 00:17:34 -0800

    updated file structure, added components, added pagesss

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Phaser from 'phaser'
+import { IonPhaser } from '@ion-phaser/react'
+
+export default function Landing() {
+    const bodyStyle={
+        height: "80vh",
+        width: "100vw",
+        overflow: "hidden",
+        backgroundColor: "black"
+    }
+
+    return (
+        <div style={bodyStyle}></div>
+    )
+}
