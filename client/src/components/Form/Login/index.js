f4e4745e031e5b011ddfb89b455f24dcb96c63e7
DJ Hersh <DanielJHersh@gmail.com>
2021-01-18 10:32:43 -0800

    Got login functionality working

--- client/src/components/Form/Login/index.js
+++ client/src/components/Form/Login/index.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from "react-router-dom";
 import { Form, Button } from 'react-bootstrap';
+import API from '../../../utils/API';
 
 function LoginForm() {
+    const [loginForm, setLoginForm] = useState({
+        username: "",
+        password: ""
+    });
+
+    const [redirect, setRedirect] = useState({
+        change: false
+    });
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        if (!loginForm.username || !loginForm.password) {
+            alert("Please provide all login information.");
+        } else {
+            API.login(loginForm)
+                .then(res => {
+                    console.log(res);
+                    if (!res.data.success) {
+                        alert(res.data.message);
+                        return;
+                    } else {
+                        setRedirect({ change: true });
+                    };
+                });
+        };
+    };
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group controlId="formBasicEmail">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="email" placeholder="Enter Username" />
+                <Form.Control type="username" name="username" placeholder="Enter Username" value={loginForm.username} onChange={e => setLoginForm({ ...loginForm, username: e.target.value })}/>
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" placeholder="Password" value={loginForm.password} onChange={e => setLoginForm({ ...loginForm, password: e.target.value })}/>
             </Form.Group>
             
-            <Button type="submit" className="nes-btn is-error mb-3" href="/harmony">
+            <Button type="submit" className="nes-btn is-error mb-3">
                 Login
             </Button>
 
-            <small class="form-text"><a href="/create" className="nes-text is-error text-decoration-none mt-4">New player? Sign up for an account here</a></small>
+            <small className="form-text"><a href="/create" className="nes-text is-error text-decoration-none mt-4">New player? Sign up for an account here</a></small>
+            {redirect.change ? <Redirect to="/harmony" /> : <Redirect to="/login" />}
         </Form> 
     );
 };
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,9 @@ import axios from "axios";
 export default {
     createUser: function(userData) {
         return axios.post("/api/user/register", userData);
+    },
+
+    login: function(userData) {
+        return axios.post("/api/user/login", userData);
     }
 };
