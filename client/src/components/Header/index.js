9b104e1b4207d0026f1982f12d8178e58038c066
DJ Hersh <DanielJHersh@gmail.com>
2021-01-18 17:43:11 -0800

    Put my authentication code into its own utils component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import Authenticate from "../../utils/Authentication";
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
 
 
 
 function Header() {
+    let loggedIn = Authenticate();
+
+    const clearStorage = () => {
+        if (loggedIn) {
+            localStorage.clear();
+        };
+    };
+
     const headerStyle = {
         height: "10vh",
         width: "100vw",
         overflowX: "hidden"
-    }
+    };
+
     return (
         <div>
             <Navbar bg="transparent" expand="lg" className="row p-4 m-0"  style={headerStyle}>
@@ -17,7 +27,7 @@ function Header() {
                     <Navbar.Brand href="/" className="p-0 m-0"><span style={{color:"red"}}>HARM</span>øny</Navbar.Brand>
                 </div>
                 <div className="col-4 text-right">
-                    <Button variant = "link" className="nes-text is-error sm text-decoration-none p-0 m-0" href="/login">Login</Button>
+                    <Button variant = "link" className="nes-text is-error sm text-decoration-none p-0 m-0" onClick={clearStorage} href={loggedIn ? "/" : "/login"}>{loggedIn ? "Logout" : "Login"}</Button>
                 </div>
             </Navbar>
         </div>
--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,29 +1,30 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
+import Authenticate from "../utils/Authentication";
 
 class ProtectedRoute extends React.Component {
     render() {
         const Component = this.props.component;
-        let isAuthenticated;
-        const parseJwt = (token) => {
-            try {
-              return JSON.parse(atob(token.split('.')[1]));
-            } catch (e) {
-              return null;
-            }
-          };
-        const token = JSON.parse(localStorage.getItem("token"));
-        if (!token) {
-            isAuthenticated = false;
-        } else {
-            const parsedToken = parseJwt(token);
-            if (Date.now() <= parsedToken.exp * 1000) {
-                isAuthenticated = true;
-            } else {
-                isAuthenticated = false;
-                localStorage.clear();
-            };
-        };
+        let isAuthenticated = Authenticate();
+        // const parseJwt = (token) => {
+        //     try {
+        //       return JSON.parse(atob(token.split('.')[1]));
+        //     } catch (e) {
+        //       return null;
+        //     }
+        //   };
+        // const token = JSON.parse(localStorage.getItem("token"));
+        // if (!token) {
+        //     isAuthenticated = false;
+        // } else {
+        //     const parsedToken = parseJwt(token);
+        //     if (Date.now() <= parsedToken.exp * 1000) {
+        //         isAuthenticated = true;
+        //     } else {
+        //         isAuthenticated = false;
+        //         localStorage.clear();
+        //     };
+        // };
        
         return isAuthenticated ? (
             <Component />
