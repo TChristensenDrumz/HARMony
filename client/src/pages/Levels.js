3497f5d016697ac00c3afafeb38de925f2c34c13
DJ Hersh <DanielJHersh@gmail.com>
2021-01-24 20:50:34 -0800

    Merging files

--- client/src/pages/Levels.js
+++ client/src/pages/Levels.js
@@ -83,3 +83,84 @@ export const BossRoom = () => {
         />
     );
 };
+
+//Edited code
+// let allGenres;
+
+// const setGenres = genres => {
+//     allGenres = genres;
+// }
+
+// LevelLogic.getGenre().then(genres => setGenres(genres));
+
+// export const Home = () => {
+//     console.log(allGenres);
+//     // console.log(LevelLogic.levelObj(allGenres.homeGenre));
+//     return (
+//         <Canvas
+//             player={Dino}
+//             enemy={Dino}
+//             bossLevel={false}
+//             enemyAmount={0}
+//             // background={background.home}
+//             // audio={audio.home}
+//             nextLevel="/harmony/level1"
+//         />
+//     );
+// };
+
+// export const Level1 = () => {
+//     return (
+//         <Canvas
+//             player={Dino}
+//             enemy={Dino}
+//             bossLevel={false}
+//             enemyAmount={3}
+//             // background={enemy.background.level1}
+//             // audio={enemy.audio.level1}
+//             nextLevel="/harmony/level2"
+//         />
+//     );
+// };
+
+// export const Level2 = () => {
+//     return (
+//         <Canvas
+//             player={Dino}
+//             enemy={Dino}
+//             bossLevel={false}
+//             enemyAmount={5}
+//             // background={background.level2}
+//             // audio={audio.level2}
+//             nextLevel="/harmony/level3"
+//         />
+//     );
+// };
+
+// export const Level3 = () => {
+//     return (
+//         <Canvas
+//             player={Dino}
+//             enemy={Dino}
+//             bossLevel={false}
+//             enemyAmount={7}
+//             // background={background.level3}
+//             // audio={audio.level3}
+//             nextLevel="/harmony/bossroom"
+//         />
+//     );
+// };
+
+// export const BossRoom = () => {
+//     return (
+//         <Canvas
+//             player={Dino}
+//             enemy={Dino}
+//             bossLevel={true}
+//             enemyAmount={1}
+//             // background={background.bossroom}
+//             // audio={audio.bossroom}
+//             nextLevel="/harmony"
+//         />
+//     );
+// };
